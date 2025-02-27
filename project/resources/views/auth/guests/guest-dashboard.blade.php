bc833afb50bb0ea6b1d21fd37dc680921431c888
hy4cinth-entice <158035130+krstinkyl@users.noreply.github.com>
2025-02-27 19:07:03 +0800

    added logout in blade

--- project/resources/views/auth/guests/guest-dashboard.blade.php
+++ project/resources/views/auth/guests/guest-dashboard.blade.php
@@ -8,5 +8,13 @@
 </head>
 <body>
     <h1>Dashboard ni Guest</h1>
+
+    <form action="{{ route('guest.logout') }}" method="POST">
+        @csrf
+        <button type="submit"
+            class="flex w-full justify-center rounded-md bg-indigo-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500">
+            Log Out
+        </button>
+    </form>
 </body>
 </html>
